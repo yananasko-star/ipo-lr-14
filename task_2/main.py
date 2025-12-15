5d8eed0e26ffefe3a7aaa6dfbb64f6021981db0e
yana <yananasko@gmail.com>
2025-12-15 17:07:56 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -1,28 +1,30 @@
 from image_lib.image_handler import ImageHandler
 from image_lib.image_processor import ImageProcessor
 
-print("=== Задание 2. Вариант 5 ===")
+def main():
+    print("=== Задание 2. Вариант 5 ===")
+    
+    # 1. Загружаем
+    path = input("Введите путь к изображению: ")
+    handler = ImageHandler(path)
+    handler.load_image()
+    
+    # 2. Масштабируем 50%
+    handler.scale_to_50_percent()
+    
+    # 3. Сохраняем с датой
+    handler.save_with_date()
+    
+    # 4. Фильтр Эмбосс
+    processor = ImageProcessor(handler.image)
+    processor.apply_emboss()
+    
+    # 5. Водяной знак
+    processor.add_watermark()
+    
+    # 6. Финальное сохранение
+    handler.image.save("final_variant5.jpg")
+    print("Готово! Файл: final_variant5.jpg")
 
-# 1. Загружаем изображение
-path = input("Введите путь к изображению: ")
-handler = ImageHandler(path)
-handler.load_image()
-
-# 2. Масштабируем до 50%
-handler.scale_to_50_percent()
-
-# 3. Сохраняем с датой
-handler.save_with_date()
-
-# 4. Применяем фильтр Эмбосс
-processor = ImageProcessor(handler.image)
-processor.apply_emboss()
-
-# 5. Добавляем водяной знак
-processor.add_watermark()
-
-# 6. Сохраняем финальный результат
-handler.image = processor.image
-handler.image.save("final_result.jpg")
-
-print("Готово! Результат в final_result.jpg")
+if name == "main":
+    main()
